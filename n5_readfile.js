1f00f7c124054f2acc9b35de40c955a65c62168f
lipeng <lipeng@daokoudai.com>
2018-06-11 21:09:01 +0800

    5.读文件:2.结合路由

--- n5_readfile.js
+++ n5_readfile.js
@@ -1,16 +1,24 @@
 var http = require('http');
-var optfile = require('./models/optfile.js');
+var url = require('url');
+var router = require('./router');
+// var optfile = require('./models/optfile.js');
 
 http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 	if(request.url !== '/favicon.ico'){ // 清除2次访问
+		var pathname = url.parse(request.url).pathname;
+		pathname = pathname.replace(/\//,'');//替换掉前面的/
+		router[pathname](request,response);
+
 		// optfile.readfileSync('./views/login.html');
+		/*
 		function recall(data){
-			response.write(data);
-			response.end('ok');	// 结束请求中状态,不写则没有http协议尾	
+			response.write()
 		}
-		optfile.readfile('./views/login.html',recall);
+		optfile.readfile('./views/login.html',response);
+		response.end('ok');	// 结束请求中状态,不写则没有http协议尾
 		console.log('主程序执行完毕');
+		*/
 	}
 }).listen(8000)
 console.log("Server running at http://127.0.0.1:8000/");
--- router.js
+++ router.js
@@ -1,8 +1,18 @@
+var optfile = require('./models/optfile.js');
+
 module.exports = {
 	login:function(req,res){
-		res.write('我是login方法')
+		function recall(data){
+			res.write(data);
+			res.end('ok');	// 结束请求中状态,不写则没有http协议尾	
+		}
+		optfile.readfile('./views/login.html',recall);
 	},
 	zhuce:function(req,res){
-		res.write('我是注册方法')
+		function recall(data){
+			res.write(data);
+			res.end('');	// 结束请求中状态,不写则没有http协议尾	
+		}
+		optfile.readfile('./views/zhuce.html',recall);
 	}
 }
