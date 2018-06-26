b416aa47f49478985c87ffc6dc279590405466b0
lipeng <lipeng@daokoudai.com>
2018-06-26 21:00:20 +0800

    9.异常处理:2.抛出异常

--- n9_exception.js
+++ n9_exception.js
@@ -1,13 +1,22 @@
 var http = require('http');
 var url = require('url');
 var router = require('./router');
+var exception = require('./models/Exception');
 http.createServer(function(request,response){
 	if(request.url !== '/favicon.ico'){ // 消除第2次访问
 		var pathname = url.parse(request.url).pathname;
 		pathname = pathname.replace(/\//,'');// 替换掉前面的/
 		try{
-			router[pathname](request,response);
-		}catch(err){// 同步错误捕获（主程序，如果异步已报错不在走这里）
+			// router[pathname](request,response);
+
+			// 抛出异常
+			// 方式一：抛出
+			data = exception.expfun(0);
+			// // 方式二：不抛出
+			// data = exception.expfun(10);
+			response.write(data);
+			response.end('');
+		}catch(err){// 同步错误捕获（主程序，如果异步已报错或已抛出异常，则不执行）
 			console.log("aaaaa="+err);
 			response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 			response.write(err.toString());
