306cc27a562c5de8eb01cb7e7d03aa493be563c2
lipeng <lipeng@daokoudai.com>
2018-06-28 17:24:34 +0800

    11.参数接收：1.get方式接收参数

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 var optfile = require('./models/optfile.js');
+var url = require('url');
+
 function getRecall(req,res){
 	res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 
@@ -12,6 +14,16 @@ function getRecall(req,res){
 
 module.exports = {
 	login:function(req,res){
+		// ---get方式接收参数---
+		
+		var rdata = url.parse(req.url,true).query;
+		console.log(rdata);
+		if(rdata['email']!=undefined){
+			console.log(rdata['email']);
+			console.log(rdata['pwd']);
+		}
+		
+
 		recall = getRecall(req,res);
 		optfile.readfile('./views/login.html',recall);
 	},
