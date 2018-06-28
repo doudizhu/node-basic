656052bbbcf72cab4fa84444283616b25890a95a
lipeng <lipeng@daokoudai.com>
2018-06-28 17:42:02 +0800

    11.参数接收：2.post方式接收参数

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 var optfile = require('./models/optfile.js');
 var url = require('url');
+var querystring = require('querystring'); // post需导入
 
 function getRecall(req,res){
 	res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
@@ -15,17 +16,32 @@ function getRecall(req,res){
 module.exports = {
 	login:function(req,res){
 		// ---get方式接收参数---
-		
+		/*
 		var rdata = url.parse(req.url,true).query;
 		console.log(rdata);
 		if(rdata['email']!=undefined){
 			console.log(rdata['email']);
 			console.log(rdata['pwd']);
 		}
-		
+		*/
+		// ---post方式接收参数---
+		var post = ''; // 定义了一个post变量，用于暂存请求体的信息
+		req.on('data',function(chunk){ // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中      
+			post += chunk
+		})
+		// 注意异步
+		req.on('end',function(){ // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+			post = querystring.parse(post);
+			console.log('收到参数：'+post['email']+'\n');
+			console.log('收到参数：'+post['pwd']+'\n');
 
-		recall = getRecall(req,res);
-		optfile.readfile('./views/login.html',recall);
+			// 方式一：
+			recall = getRecall(req,res);
+			optfile.readfile('./views/login.html',recall);
+		})
+		// // 方式二：
+		// recall = getRecall(req,res);
+		// optfile.readfile('./views/login.html',recall);
 	},
 	zhuce:function(req,res){
 		recall = getRecall(req,res);
