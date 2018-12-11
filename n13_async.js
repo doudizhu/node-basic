7000127212764583cd2444784f7f2761cef68a77
lipeng <lipeng@daokoudai.com>
2018-12-11 15:54:09 +0800

    15.异步流程控制（下）并行无关联parallel

--- n13_async.js
+++ n13_async.js
@@ -34,11 +34,51 @@ console.log("主进程执行完毕");
 */
 
 
-
+/*
 // 2.异步：串行无关联
 var async = require('async');
 function exec(){
 	async.series(
+		{
+			one:function(done){
+				ii=0;
+				setInterval(function(){
+					console.log("aaa="+new Date());
+					ii++;
+					if(ii==3){
+						clearInterval(this);
+						done(null,'one完毕')
+					}
+				}, 1000);
+			},
+			two:function(done){
+				jj=0;
+				setInterval(function(){
+					console.log("bbb="+new Date());
+					jj++;
+					if(jj==3){
+						clearInterval(this);
+						done(null,'two完毕')
+					}
+				}, 1000);
+			}
+		},
+		// 回调结果集
+		function(err,rs){
+			console.log(err);
+			console.log(rs);
+		}
+	)
+}
+exec();
+console.log("主进程执行完毕");
+*/
+
+
+// 3.异步：并行无关联
+var async = require('async');
+function exec(){
+	async.parallel(
 		{
 			one:function(done){
 				ii=0;
